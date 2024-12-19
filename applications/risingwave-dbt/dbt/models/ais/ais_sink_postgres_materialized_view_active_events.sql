0bc74d28e492108871ae073d990aa8bcadbe199e
Marcel Maatkamp <m.maatkamp@gmail.com>
2024-12-19 20:45:31 +0100

    almost

--- applications/risingwave-dbt/dbt/models/ais/ais_sink_postgres_materialized_view_active_events.sql
+++ applications/risingwave-dbt/dbt/models/ais/ais_sink_postgres_materialized_view_active_events.sql
@@ -4,7 +4,7 @@ FROM {{ ref('ais_materialized_view_active_events') }}
 WITH (
     connector = 'jdbc',
     jdbc.url = 'jdbc:postgresql://postgres-data/data?user=postgres&password=postgres',
-    table.name = 'ais_materialized_view_active_events',
+    table.name = 'ais_position',
     type = 'upsert',
     primary_key = 'mmsi'
 );
