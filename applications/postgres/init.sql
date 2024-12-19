454a7f375d6fbcc7fa6ccca7bf41386039df83cf
Marcel Maatkamp <m.maatkamp@gmail.com>
2024-12-19 14:15:19 +0100

    imnit

--- applications/postgres/init.sql
+++ applications/postgres/init.sql
@@ -1,1 +1,18 @@
 CREATE EXTENSION postgres_fdw;
+
+CREATE SERVER risingwave
+        FOREIGN DATA WRAPPER postgres_fdw
+        OPTIONS (
+            host 'risingwave',
+            port '4566', 
+            dbname 'dev');
+
+CREATE USER MAPPING FOR postgres
+        SERVER risingwave
+        OPTIONS (
+            user 'root', 
+            password 'root');
+
+IMPORT FOREIGN SCHEMA public
+    FROM SERVER risingwave 
+        INTO public;
